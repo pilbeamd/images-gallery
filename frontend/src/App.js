f3ff005ee7869406d996b38ba58005d9284ba7bc
Dominic Pilbeam <dominic@viableoptions.ie>
2023-05-07 12:09:27 +0100

    latest

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,33 @@
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState("");
+  const [images, setImages] = useState([]);
+
+  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
+        console.log(images);
       })
       .catch((err) => {
         console.log(err);
       });
 
-      setWord('');
-  }
+    setWord("");
+  };
 
   return (
     <div className="App">
